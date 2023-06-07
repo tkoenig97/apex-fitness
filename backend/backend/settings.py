b48df6715da1d9921ae7dc6125304dcc8edd406e
tkoenig97 <tylerskoenig@gmail.com>
2023-06-07 14:21:59 -0500

    Migrated user model

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -125,4 +125,4 @@ STATICFILES_DIRS = [ BASE_DIR / 'static']
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# AUTH_USER_MODEL = 'fitness_app.App_User'
+AUTH_USER_MODEL = 'fitness_app.App_User'
