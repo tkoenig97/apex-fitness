da72a38489e68e1c4b8f4572e4251e54fb8f5e9d
tkoenig97 <tylerskoenig@gmail.com>
2023-06-07 14:05:26 -0500

    Set up urls files

---
+++ backend/fitness_app/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.react_index),
+    # path('signup/', views.user_sign_up, name='signup'),
+    # path('login/', views.user_log_in, name= 'login'),
+    # path('curruser/', views.curr_user, name= 'curruser'),
+    # path('logout/', views.user_log_out, name= 'logout'),
+]
