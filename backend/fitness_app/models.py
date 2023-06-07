648b71cbf08cfc3fc7df833c3250e5405e4f35ed
tkoenig97 <tylerskoenig@gmail.com>
2023-06-07 14:17:27 -0500

    Defined user model

--- backend/fitness_app/models.py
+++ backend/fitness_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class App_user(AbstractUser):
+    email = models.EmailField(blank = False, null = False, unique = True)
+    # Assigns email as username
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+    
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} | {self.email}"
+    
