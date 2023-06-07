2a2ce3767151a737693df73a80667630f62c8232
tkoenig97 <tylerskoenig@gmail.com>
2023-06-07 14:13:54 -0500

    Sucessfully connected React and Django

--- backend/fitness_app/views.py
+++ backend/fitness_app/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 # Create your views here.
 def react_index(request):
     # Grabs index from React app
-    index = open('static/index.html')
+    index = open('../static/index.html')
     return HttpResponse(index)
