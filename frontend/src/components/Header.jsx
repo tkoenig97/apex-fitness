9e7224098c8ae501abb5bc739b6a130997adeabb
tkoenig97 <tylerskoenig@gmail.com>
2023-06-09 15:50:19 -0500

    Made the site header its own component

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,27 @@
+import { toggleMenu } from "../utils/NavUtils";
+
+export const Header = () => {
+    return (
+        <div className="px-4 flex md:justify-center justify-between items-center">
+            <h1 className="text-4xl hover:cursor-pointer hover:text-brown-300">
+                Apex Fitness
+            </h1>
+            <div className="cursor-pointer md:hidden" onClick={toggleMenu}>
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                >
+                    <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                    />
+                </svg>
+            </div>
+        </div>
+    );
+};
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,31 +1,10 @@
 import { Link } from 'react-router-dom';
-import { toggleMenu } from '../utils/NavUtils';
+import { Header } from './Header';
 
 export const Navbar = () => {
     return (
         <div className="bg-brown-500 col-span-1 text-brown-200 ">
-            <div className='px-4 flex md:justify-center justify-between items-center'>
-                <h1>Apex Fitness</h1>
-                <div
-                    className="cursor-pointer md:hidden"
-                    onClick={toggleMenu}
-                >
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth={1.5}
-                        stroke="currentColor"
-                        className="w-6 h-6"
-                    >
-                        <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-                        />
-                    </svg>
-                </div>
-            </div>
+            <Header />
             <div className="flex justify-center">
                 <ul
                     className="text-2xl justify-between hidden md:block"
