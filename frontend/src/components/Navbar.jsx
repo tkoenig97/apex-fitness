a04ae25782b6bb03c86ece07eeb3a83b681aa2cd
tkoenig97 <tylerskoenig@gmail.com>
2023-06-09 15:53:34 -0500

    Removed div from ul

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,28 +5,26 @@ export const Navbar = () => {
     return (
         <div className="bg-brown-500 col-span-1 text-brown-200 ">
             <Header />
-            <div className="flex justify-center">
-                <ul
-                    className="text-2xl justify-between hidden md:block"
-                    id="nav"
-                >
-                    <li className="mt-6">
-                        <Link to={'/'}>Home</Link>
-                    </li>
-                    <li className="mt-6">
-                        <Link to={'/routines'}>Routines</Link>
-                    </li>
-                    <li className="mt-6">
-                        <Link to={'/interval'}>Interval Training</Link>
-                    </li>
-                    <li className="mt-6">
-                        <Link to={'/progress'}>Progress</Link>
-                    </li>
-                    <li className="mt-6">
-                        <Link to={'/log'}>Daily Log</Link>
-                    </li>
-                </ul>
-            </div>
+            <ul
+                className="flex flex-col justify-center text-2xl hidden md:block"
+                id="nav"
+            >
+                <li className="mt-6">
+                    <Link to={'/'}>Home</Link>
+                </li>
+                <li className="mt-6">
+                    <Link to={'/routines'}>Routines</Link>
+                </li>
+                <li className="mt-6">
+                    <Link to={'/interval'}>Interval Training</Link>
+                </li>
+                <li className="mt-6">
+                    <Link to={'/progress'}>Progress</Link>
+                </li>
+                <li className="mt-6">
+                    <Link to={'/log'}>Daily Log</Link>
+                </li>
+            </ul>
         </div>
     );
 };
