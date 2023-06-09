34ba8b832b30fb4d04db7c8b88a4ac4e8de400d4
tkoenig97 <tylerskoenig@gmail.com>
2023-06-09 15:11:23 -0500

    Working on nav for small screens

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,16 @@
-import { Outlet } from 'react-router-dom'
-import { Navbar } from './components/Navbar'
-import './App.css'
+import { Outlet } from 'react-router-dom';
+import { Navbar } from './components/Navbar';
+import './App.css';
 
 function App() {
-  return (
-    <div>
-      <Navbar />
-      <Outlet />
-    </div>
-  )
+    return (
+        <div className="md:grid grid-cols-4">
+            <Navbar />
+            <div className="col-span-3 text-center bg-gray-300">
+                <Outlet />
+            </div>
+        </div>
+    );
 }
 
-export default App
+export default App;
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,12 +2,41 @@ import { Link } from 'react-router-dom';
 
 export const Navbar = () => {
     return (
-        <div className="navbar">
-            <Link to={'/'}></Link>
-            <Link to={'/routines'}>Routines</Link>
-            <Link to={'/interval'}>Interval Training</Link>
-            <Link to={'/progress'}>Progress</Link>
-            <Link to={'/log'}>Daily Log</Link>
+        <div className="flex justify-between items-center bg-brown-500 col-span-1 text-brown-200">
+            <h1>Apex Fitness</h1>
+            <div className="px-4 cursor-pointer md:hidden">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                >
+                    <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                    />
+                </svg>
+            </div>
+            <ul className="text-2xl flex justify-between hidden md:block">
+                <li className="mt-6">
+                    <Link to={'/'}>Home</Link>
+                </li>
+                <li className="mt-6">
+                    <Link to={'/routines'}>Routines</Link>
+                </li>
+                <li className="mt-6">
+                    <Link to={'/interval'}>Interval Training</Link>
+                </li>
+                <li className="mt-6">
+                    <Link to={'/progress'}>Progress</Link>
+                </li>
+                <li className="mt-6">
+                    <Link to={'/log'}>Daily Log</Link>
+                </li>
+            </ul>
         </div>
     );
 };
