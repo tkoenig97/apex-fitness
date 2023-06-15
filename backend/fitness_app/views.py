a661c38bb98708285cf4bddc0b2105a0f0b5fcae
tkoenig97 <tylerskoenig@gmail.com>
2023-06-15 13:36:36 -0500

    Created sign up view

--- backend/fitness_app/views.py
+++ backend/fitness_app/views.py
@@ -1,7 +1,30 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from rest_framework.decorators import api_view
+from .models import App_User
 
 # Create your views here.
 def react_index(request, route=None):
     # Grabs index from React app
     index = open('../static/index.html')
     return HttpResponse(index)
+
+@api_view(['POST'])
+def user_sign_up(request):
+    email = request.data['email']
+    password = request.data['password']
+    first_name = request.data['firstName']
+    last_name = request.data['lastName']
+    super_user = False
+    staff = False
+    if 'super' in request.data:
+        super_user = request.data['super']
+    if 'staff' in request.data:
+        staff = request.data['staff']
+    try:
+        # Creates new user
+        new_user = App_User.objects.create_user(username = email, email = email, first_name = first_name, last_name = last_name, password = password, is_superuser = super_user, is_staff = staff)
+        new_user.save()
+        return JsonResponse({'success':True})
+    except Exception as e:
+        print(e)
+        return JsonResponse({'success':False})
