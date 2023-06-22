7501b675f7cbd71fa92665ffa2b330d05826d3c6
tkoenig97 <tylerskoenig@gmail.com>
2023-06-22 15:23:19 -0500

    Completed functionality of BRM calculator

--- frontend/src/components/bmr/BmrForm.jsx
+++ frontend/src/components/bmr/BmrForm.jsx
@@ -1,17 +1,63 @@
+import { useContext, useState } from 'react';
+import { BmrContext } from '../../pages/BmrPage';
+import { CalculateBmr, convertFtToIn } from '../../utils/BmrUtils';
+
 export const BmrForm = () => {
+    const { setBmr } = useContext(BmrContext);
+    const [age, setAge] = useState(0);
+    const [gender, setGender] = useState('');
+    const [heightFt, setHeightFt] = useState(0);
+    const [heightIn, setHeightIn] = useState(0);
+    const [weight, setWeight] = useState(0);
+    const [tenthWeight, setTenthWeight] = useState(0);
+
+    const handleFormSubmit = (event) => {
+        event.preventDefault(); // Prevent the default form submission behavior
+
+        // Call the conversion function and CalculateBmr with the form values
+        const convertedHeight = convertFtToIn(heightFt, heightIn);
+        const calculatedBmr = CalculateBmr(
+            age,
+            gender,
+            convertedHeight,
+            weight + tenthWeight / 10
+        );
+
+        // Update the BmrContext with the calculated BMR
+        setBmr(calculatedBmr);
+    };
+
     return (
         <div>
-            <form className="flex flex-col my-8">
+            <form className="flex flex-col my-8" onSubmit={handleFormSubmit}>
                 <div className="my-2">
                     <label>Age: </label>
-                    <input type="number" min="1" max="100"></input>
+                    <input
+                        type="number"
+                        min="1"
+                        max="100"
+                        value={age}
+                        onChange={(e) => setAge(parseInt(e.target.value))}
+                    ></input>
                 </div>
                 <label>Gender:</label>
                 <div className="my-2">
-                    <input type="radio" className="mx-2" />
+                    <input
+                        type="radio"
+                        className="mx-2"
+                        value="male"
+                        checked={gender === 'male'}
+                        onChange={() => setGender('male')}
+                    />
                     <label>Male</label>
 
-                    <input type="radio" className="mx-2" />
+                    <input
+                        type="radio"
+                        className="mx-2"
+                        value="female"
+                        checked={gender === 'female'}
+                        onChange={() => setGender('female')}
+                    />
                     <label>Female</label>
                 </div>
                 <div className="my-2 flex justify-center">
@@ -21,6 +67,8 @@ export const BmrForm = () => {
                         type="number"
                         min="1"
                         max="10"
+                        value={heightFt}
+                        onChange={(e) => setHeightFt(parseInt(e.target.value))}
                     ></input>
                     <h2>ft</h2>
                     <input
@@ -28,6 +76,8 @@ export const BmrForm = () => {
                         type="number"
                         min="0"
                         max="11"
+                        value={heightIn}
+                        onChange={(e) => setHeightIn(parseInt(e.target.value))}
                     ></input>
                     <h2>in</h2>
                 </div>
@@ -38,6 +88,8 @@ export const BmrForm = () => {
                         type="number"
                         min="0"
                         max="1000"
+                        value={weight}
+                        onChange={(e) => setWeight(parseInt(e.target.value))}
                     ></input>
                     <h2>.</h2>
                     <input
@@ -45,6 +97,10 @@ export const BmrForm = () => {
                         type="number"
                         min="0"
                         max="9"
+                        value={tenthWeight}
+                        onChange={(e) =>
+                            setTenthWeight(parseInt(e.target.value))
+                        }
                     ></input>
                     <h2>pounds</h2>
                 </div>
--- frontend/src/pages/BmrPage.jsx
+++ frontend/src/pages/BmrPage.jsx
@@ -1,10 +1,18 @@
+import { createContext, useState } from 'react';
 import { BmrForm } from '../components/bmr/BmrForm';
 
+export const BmrContext = createContext(null);
+
 export const BmrPage = () => {
+    const [bmr, setBmr] = useState(0);
+
     return (
         <div>
-            <h2 className='text-2xl my-5'>BMR Calculator</h2>
-            <BmrForm />
+            <h2 className="text-2xl my-5">BMR Calculator</h2>
+            <BmrContext.Provider value={{ bmr, setBmr }}>
+                <BmrForm />
+            </BmrContext.Provider>
+            <h2>{ bmr } Calories</h2>
         </div>
     );
 };
