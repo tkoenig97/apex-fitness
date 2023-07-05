14aef33bf1b6605ea78d0a4dac457b14245c73dc
tkoenig97 <tylerskoenig@gmail.com>
2023-07-05 15:06:24 -0500

    Created models for exercises and routines

--- backend/fitness_app/models.py
+++ backend/fitness_app/models.py
@@ -11,3 +11,25 @@ class App_user(AbstractUser):
     def __str__(self):
         return f"{self.first_name} {self.last_name} | {self.email}"
     
+class Exercises(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    muscle = models.CharField(max_length=255)
+    description = models.TextField()
+
+class Routines(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey('Users', on_delete=models.CASCADE)
+    type = models.CharField(max_length=255)
+
+class RoutineExercises(models.Model):
+    workout = models.ForeignKey('Routines', on_delete=models.CASCADE)
+    routine = models.ForeignKey('Exercises', on_delete=models.CASCADE)
+    reps = models.IntegerField(null=True)
+    rest_duration = models.IntegerField()
+    sets = models.IntegerField()
+    weight = models.IntegerField(null=True)
+
+class Users(models.Model):
+    id = models.BigAutoField(primary_key=True)
