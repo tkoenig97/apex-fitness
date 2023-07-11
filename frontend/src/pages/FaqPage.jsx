bdb9f2d554be9aa032c354874ddada86aee57a6b
tkoenig97 <tylerskoenig@gmail.com>
2023-07-11 12:44:14 -0500

    Created temp object to render accordion and placed current questions and answers in a txt file

--- frontend/src/pages/FaqPage.jsx
+++ frontend/src/pages/FaqPage.jsx
@@ -1,5 +1,22 @@
+import { FaqAccordion } from '../components/faq/FaqAccordion';
+
 export const FaqPage = () => {
+    // Test object for accordion component
+    const faqs = [
+        {
+            question: 'How many days a week should I work out?',
+            answer: 'The recommended frequency is at least 3-5 days per week. However, it depends on your goals, fitness level, and availability. Remember to include both cardio and strength training exercises.',
+        },
+        {
+            question: 'What is the best exercise for weight loss?',
+            answer: "There isn't a single best exercise for weight loss. A combination of cardio exercises (e.g., running, cycling) and strength training (e.g., weightlifting) can help you burn calories, build muscle, and achieve weight loss goals.",
+        },
+    ];
+
     return (
-        <h1>Frequently Asked Questions</h1>
-    )
-}
+        <div>
+            <h1>Frequently Asked Questions</h1>
+            <FaqAccordion faqs={faqs} />
+        </div>
+    );
+};
