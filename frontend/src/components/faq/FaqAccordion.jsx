5cf7a8f7a630fae29c3fd59e690a7f77019155d2
tkoenig97 <tylerskoenig@gmail.com>
2023-07-11 12:57:49 -0500

    Open transition not functioning and added comments for readability

--- frontend/src/components/faq/FaqAccordion.jsx
+++ frontend/src/components/faq/FaqAccordion.jsx
@@ -2,31 +2,38 @@ import { useState } from 'react';
 import PropTypes from 'prop-types';
 
 export const FaqAccordion = ({ faqs }) => {
+    // State to track which question is opened
     const [activeIndex, setActiveIndex] = useState(null);
 
+    // Toggles question to open on click or close if another is opened
     const toggleAccordion = (index) => {
         setActiveIndex(activeIndex === index ? null : index);
     };
 
     return (
         <div>
+            {/* Displays an Accordion item for each item in faqs object */}
             {faqs.map((faq, index) => (
                 <div key={index} className="border-b">
+                    {/* Accordion header */}
                     <div
                         className={`flex justify-between items-center py-3 px-4 cursor-pointer ${
                             activeIndex === index
                                 ? 'bg-gray-200'
                                 : 'bg-gray-100'
-                        }`}
+                        } transition duration-1000 ease-in-out`}
                         onClick={() => toggleAccordion(index)}
                     >
+                        {/* FAQ question */}
                         <h3 className="text-lg font-semibold">
                             {faq.question}
                         </h3>
+                        {/* Accordion icon (Indicates open or closed) */}
                         <span className="accordion-icon">
                             {activeIndex === index ? '-' : '+'}
                         </span>
                     </div>
+                    {/* Accordion content */}
                     {activeIndex === index && (
                         <div className="bg-gray-100 p-4">
                             <p>{faq.answer}</p>
